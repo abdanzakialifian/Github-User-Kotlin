c51fce784c6cfff748a1a5b465b2e06fc9cb0706
Abdan Alifian <abdan.alifian@mncbank.co.id>
2023-02-22 15:52:30 +0700

    Add timber to code

--- app/src/main/java/com/application/zaki/githubuser/MyApplication.kt
+++ app/src/main/java/com/application/zaki/githubuser/MyApplication.kt
@@ -2,6 +2,15 @@ package com.application.zaki.githubuser
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class MyApplication : Application()
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            // timber logging
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
